04f54c15457fccd5811723b79a226d79c6672777
Benjamin <benjaminesims@gmail.com>
2026-05-02 20:33:54 -0600

    Modernized project

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const core = require("@actions/core")
-const tc = require("@actions/tool-cache")
-const path = require("path")
+import * as core from "@actions/core";
+import * as tc from "@actions/tool-cache";
+import path from "node:path";
 
-async function main() {
+try {
     const version = core.getInput('version', { required: true })
     const userPath = core.getInput('path', { required: false })
-    if (userPath[0] == '/' || userPath[0] == '~') {
+
+    if (userPath.startsWith('/') || userPath.startsWith('~')) {
         throw new Error("Path must be relative to the workspace")
     }
+
     const pathPrefix = "https://github.com/premake/premake-core/releases/download/" + "v" + version + "/premake-" + version
-    const premakePath = path.join(process.env.GITHUB_WORKSPACE, userPath)
-    if (process.platform == "win32") {
+    const workspace = process.env.GITHUB_WORKSPACE || process.cwd()
+    const premakePath = path.join(workspace, userPath)
+
+    if (process.platform === "win32") {
         const premake = await tc.downloadTool(pathPrefix + "-windows.zip")
         await tc.extractZip(premake, premakePath)
     }
-    else if (process.platform == "darwin") {
+    else if (process.platform === "darwin") {
         const premake = await tc.downloadTool(pathPrefix + "-macosx.tar.gz")
         await tc.extractTar(premake, premakePath)
     }
@@ -22,9 +26,8 @@ async function main() {
         const premake = await tc.downloadTool(pathPrefix + "-linux.tar.gz")
         await tc.extractTar(premake, premakePath)
     }
-    core.addPath(premakePath)
-}
 
-main().catch(err => {
+    core.addPath(premakePath)
+} catch (err) {
     core.setFailed(`Failed to install premake: ${err}`);
-})
+}
